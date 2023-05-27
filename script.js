c113abde739179efd3fb2b057a53bfe6efb67d06
hanifa <hanifatavassoli@gmail.com>
2023-05-27 23:43:17 +0430

    manipulating the game logic

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('No Number');
+
+    // when the player wins
+  } else if (secretNumber === guess) {
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    displayMessage('Correct Number');
+    document.querySelector('body').style.background = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = guess;
+
+    // when the guess is different
+  } else if (secretNumber !== guess) {
+    if (score > 1) {
+      displayMessage(
+        secretNumber > guess ? 'Your guess is low!' : 'Your guess is high!'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('Lost the Game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
